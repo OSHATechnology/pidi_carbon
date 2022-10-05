07f8ebf20bbe2cf6cd608c20a68a2cfa5333986a
rizky <rizkyaditya2106@gmail.com>
2022-10-05 13:38:06 +0700

    adjusting charts

--- public/assets/js/custom-latest.js
+++ public/assets/js/custom-latest.js
@@ -1,50 +1,85 @@
 // chart1
 var options = {
   series: [{
-  name: 'CO2 Emmisions',
-  data: [44, 55, 41, 37, 22, 43, 21]
+  name: 'CO2 Emission',
+  type: 'column',
+  data: [10, 22, 25, 15, 25, 28, 38, 46]
 }, {
-  name: 'CO2 Tax',
-  data: [53, 32, 33, 52, 13, 43, 32]
+  name: 'Tax',
+  type: 'line',
+  data: [20, 29, 37, 36, 44, 45, 50, 58]
 }],
-  chart: {
-  type: 'bar',
+chart: {
   height: 350,
-  stacked: true,
+  type: 'line',
+  stacked: false,
+  toolbar: false
 },
-plotOptions: {
-  bar: {
-    horizontal: true,
-  },
+dataLabels: {
+  enabled: false
 },
 stroke: {
-  width: 1,
-  colors: ['#fff']
+  width: [1, 1, 4]
 },
 xaxis: {
-  categories: [2008, 2009, 2010, 2011, 2012, 2013, 2014],
-  labels: {
-    formatter: function (val) {
-      return val + "K"
-    }
-  }
+  categories: [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016],
 },
 yaxis: [
+  {
+    axisTicks: {
+      show: true,
+    },
+    axisBorder: {
+      show: true,
+      color: '#008FFB'
+    },
+    labels: {
+      style: {
+        colors: '#008FFB',
+      }
+    },
+    title: {
+      text: "CO2 Emission/TON",
+      style: {
+        color: '#008FFB',
+      }
+    },
+    tooltip: {
+      enabled: true
+    }
+  },
+  {
+    seriesName: 'tax',
+    opposite: true,
+    axisTicks: {
+      show: true,
+    },
+    axisBorder: {
+      show: true,
+      color: '#FEB019'
+    },
+    labels: {
+      style: {
+        colors: '#FEB019',
+      },
+    },
+    title: {
+      text: "Tax Rp. (Juta)",
+      style: {
+        color: '#FEB019',
+      }
+    }
+  },
 ],
 tooltip: {
-  y: {
-    formatter: function (val) {
-      return val + "K"
-    }
-  }
+  fixed: {
+    enabled: true,
+    position: 'topLeft', // topRight, topLeft, bottomRight, bottomLeft
+    offsetY: 30,
+    offsetX: 60
+  },
 },
-fill: {
-  opacity: 1
-},
-legend: {
-  position: 'top',
-  horizontalAlign: 'center'
-}
+colors: ['#008FFB','#FEB019']
 };
 
 var chart = new ApexCharts(document.querySelector("#chart1"), options);
@@ -53,132 +88,96 @@ chart.render();
 // chart2
 var options = {
   series: [{
-  name: 'PRODUCT A',
-  data: [44, 55, 41, 67, 22, 43]
-}, {
-  name: 'PRODUCT B',
-  data: [13, 23, 20, 8, 13, 27]
-}, {
-  name: 'PRODUCT C',
-  data: [11, 17, 15, 15, 21, 14]
-}, {
-  name: 'PRODUCT D',
-  data: [21, 7, 25, 13, 22, 8]
-},
-{
-  name: 'PRODUCT E',
-  data: [23, 7, 25, 13, 22, 8]
-}],
-chart: {
-  type: 'bar',
-  height: 350,
-  stacked: true,
-  toolbar: {
-    show: true
-  },
-  zoom: {
-    enabled: true
-  }
-},
-legend: {
-  position: 'top'
-},
-responsive: [{
-  breakpoint: 480,
-  options: {
-    legend: {
-      position: 'bottom',
-      offsetX: -10,
-      offsetY: 0
+    name: 'Welding',
+    data: [44, 55, 41, 67, 22, 43]
+  }, {
+    name: 'Painting',
+    data: [13, 23, 20, 8, 13, 27]
+  }, {
+    name: 'Press',
+    data: [11, 17, 15, 15, 21, 14]
+  }, {
+    name: 'Assy',
+    data: [21, 7, 25, 13, 22, 8]
+  }],
+  chart: {
+    type: 'bar',
+    height: 350,
+    stacked: true,
+    toolbar: {
+      show: true
+    },
+    zoom: {
+      enabled: true
+    },
+    toolbar: {
+      show: false,
     }
-  }
-}],
-plotOptions: {
-  bar: {
-    horizontal: false,
-    borderRadius: 10
   },
-},
-xaxis: {
-  type: 'datetime',
-  categories: ['01/01/2011 GMT', '01/02/2011 GMT', '01/03/2011 GMT', '01/04/2011 GMT',
-    '01/05/2011 GMT', '01/06/2011 GMT'
-  ],
-},
-fill: {
-  opacity: 1
-}
+  legend: {
+    position: 'top'
+  },
+  plotOptions: {
+    bar: {
+      horizontal: false,
+      borderRadius: 10
+    },
+  },
+  xaxis: {
+    type: 'datetime',
+    categories: ['01/01/2011 GMT', '01/02/2011 GMT', '01/03/2011 GMT', '01/04/2011 GMT',
+      '01/05/2011 GMT', '01/06/2011 GMT'
+    ],
+  },
+  fill: {
+    opacity: 1
+  }
 };
 
 var chart = new ApexCharts(document.querySelector("#chart2"), options);
 chart.render();
 
+// chart3
 var options = {
   series: [44, 55, 41, 17, 15],
+  labels: ['Plant 1', 'Plant 2', 'Plant 3', 'Plant 4', 'Plant 5'],
   chart: {
-  type: 'donut',
-},
-legend: {
-  position: 'bottom'
-},
-responsive: [{
-  breakpoint: 480,
-  options: {
-    chart: {
-      width: 200
-    },
-    legend: {
-      position: 'bottom'
-    }
+    type: 'donut',
+  },
+  dataLabels: {
+    enabled: false
+  },
+  legend: {
+    position: 'bottom',
+    itemMargin: {
+      horizontal: 10,
+      vertical: 5
+    } 
   }
-}]
 };
 
 var chart = new ApexCharts(document.querySelector("#chart3"), options);
 chart.render();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Chart4
 var options = {
   series: [88, 32],
   chart: {
-  width: 380,
-  type: 'pie',
-},
-labels: ['Total Emission', 'Target'],
-responsive: [{
-  breakpoint: 480,
-  options: {
-    chart: {
-      width: 200
-    },
-    legend: {
-      position: 'bottom'
+    width: 380,
+    type: 'pie',
+  },
+  labels: ['Total Emission', 'Target'],
+  responsive: [{
+    breakpoint: 480,
+    options: {
+      chart: {
+        width: 200
+      },
+      legend: {
+        position: 'bottom'
+      }
     }
-  }
-}]
+  }]
 };
 
 var chart = new ApexCharts(document.querySelector("#chart4"), options);
@@ -187,18 +186,18 @@ chart.render();
 // Chart5
 var options = {
   series: [{
-  data: generateDayWiseTimeSeries(new Date('11 Feb 2017').getTime(), 20, {
-    min: 10,
-    max: 60
-  })
-}],
+    data: generateDayWiseTimeSeries(new Date('11 Feb 2017').getTime(), 10, {
+      min: 10,
+      max: 60
+    })
+  }],
   chart: {
-  id: 'fb',
-  group: 'social',
-  type: 'line',
-  height: 160
-},
-colors: ['#008FFB']
+    id: 'fb',
+    group: 'social',
+    type: 'line',
+    height: 260
+  },
+  colors: ['#008FFB']
 };
 
 var chart = new ApexCharts(document.querySelector("#chart-line"), options);
@@ -206,18 +205,18 @@ chart.render();
 
 var optionsLine2 = {
   series: [{
-  data: generateDayWiseTimeSeries(new Date('11 Feb 2017').getTime(), 20, {
-    min: 10,
-    max: 30
-  })
-}],
+    data: generateDayWiseTimeSeries(new Date('11 Feb 2017').getTime(), 10, {
+      min: 10,
+      max: 60
+    })
+  }],
   chart: {
-  id: 'tw',
-  group: 'social',
-  type: 'line',
-  height: 160
-},
-colors: ['#546E7A']
+    id: 'tw',
+    group: 'social',
+    type: 'line',
+    height: 260
+  },
+  colors: ['#546E7A']
 };
 
 var chartLine2 = new ApexCharts(document.querySelector("#chart-line2"), optionsLine2);
@@ -225,18 +224,18 @@ chartLine2.render();
 
 var optionsArea = {
   series: [{
-  data: generateDayWiseTimeSeries(new Date('11 Feb 2017').getTime(), 20, {
-    min: 10,
-    max: 60
-  })
-}],
+    data: generateDayWiseTimeSeries(new Date('11 Feb 2017').getTime(), 10, {
+      min: 10,
+      max: 60
+    })
+  }],
   chart: {
-  id: 'yt',
-  group: 'social',
-  type: 'area',
-  height: 160
-},
-colors: ['#00E396']
+    id: 'yt',
+    group: 'social',
+    type: 'area',
+    height: 260
+  },
+  colors: ['#00E396']
 };
 
 var chartArea = new ApexCharts(document.querySelector("#chart-area"), optionsArea);
@@ -244,19 +243,18 @@ chartArea.render();
 
 var optionsSmall = {
   series: [{
-  data: generateDayWiseTimeSeries(new Date('11 Feb 2017').getTime(), 20, {
-    min: 10,
-    max: 60
-  })
-}],
+    data: generateDayWiseTimeSeries(new Date('11 Feb 2017').getTime(), 10, {
+      min: 10,
+      max: 60
+    })
+  }],
   chart: {
-  id: 'ig',
-  group: 'social',
-  type: 'area',
-  height: 160,
-  width: 300
-},
-colors: ['#008FFB']
+    id: 'ig',
+    group: 'social',
+    type: 'area',
+    height: 260,
+  },
+  colors: ['#008FFB']
 };
 
 var chartSmall = new ApexCharts(document.querySelector("#chart-small"), optionsSmall);
@@ -264,7 +262,7 @@ chartSmall.render();
 
 var optionsSmall2 = {
   series: [{
-  data: generateDayWiseTimeSeries(new Date('11 Feb 2017').getTime(), 20, {
+  data: generateDayWiseTimeSeries(new Date('11 Feb 2017').getTime(), 10, {
     min: 10,
     max: 60
   })
@@ -273,8 +271,7 @@ var optionsSmall2 = {
   id: 'li',
   group: 'social',
   type: 'area',
-  height: 160,
-  width: 300
+  height: 260,
 },
 colors: ['#546E7A']
 };
@@ -283,3 +280,19 @@ var chartSmall2 = new ApexCharts(document.querySelector("#chart-small2"), option
 chartSmall2.render();
 
 // Chart6
+
+
+//Define a method to simulate data, this is the method of ApexCharts official website 
+function  generateDayWiseTimeSeries ( baseval , count , yrange )  {  
+  var i =  0 ; 
+  var series =  [ ] ; 
+  while  ( i < count )  { 
+    var x = baseval ; 
+    var y = Math.floor( Math.random()*(yrange.max-yrange.min+.1 ))+yrange.min;
+
+    series.push([x,y]) ; 
+    baseval +=  86400000 ; 
+    i++ ; 
+  } 
+  return series ; 
+}
--- resources/views/components-latest/dashboard.blade.php
+++ resources/views/components-latest/dashboard.blade.php
@@ -12,7 +12,17 @@
             <div class="col-3">
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">CO2 Tax</h5>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <h5 class="card-title mb-0">CO2 Tax</h5>
+                            <div>
+                                <select name="" id="" class="form-select form-select-sm">
+                                    All
+                                    <option value="plant1">Plant 1</option>
+                                    <option value="plant2">Plant 2</option>
+                                    <option value="plant3">Plant 3</option>
+                                </select>
+                            </div>
+                        </div>
                         <div id="chart1"></div>
                     </div>
                 </div>
@@ -31,181 +41,53 @@
             <div class="col-3">
                 <div class="card">
                     <div class="card-body">
-                        <div id="chart3"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-
-        {{-- ROW 2 --}}
-        {{--  --}}
-        <div class="row my-3">
-            <div class="col">
-                <div class="card with-border">
-                    <div class="card-body">
-                        <div class="card-title d-flex justify-content-between">
-                            <p class="small normal lighter">CO2 Emission</p>
-                            <p class="small normal">Data per 1 October 2022</p>
-                        </div>
-                        <div id="chart2"></div>
-                        <h5 class="text-center text-success">Painting</h5>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card with-border">
-                    <div class="card-body">
-                        <div class="card-title d-flex justify-content-between">
-                            <p class="small normal lighter">CO2 Emission</p>
-                            <p class="small normal">Data per 1 October 2022</p>
-                        </div>
-                        <div id="chart3"></div>
-                        <h5 class="text-center text-success">Welding</h5>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card with-border">
-                    <div class="card-body">
-                        <div class="card-title d-flex justify-content-between">
-                            <p class="small normal lighter">CO2 Emission</p>
-                            <p class="small normal">Data per 1 October 2022</p>
-                        </div>
-                        <div id="chart4"></div>
-                        <h5 class="text-center text-success">Press</h5>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card with-border">
-                    <div class="card-body">
-                        <div class="card-title d-flex justify-content-between">
-                            <p class="small normal lighter">CO2 Emission</p>
-                            <p class="small normal">Data per 1 October 2022</p>
-                        </div>
-                        <div id="chart5"></div>
-                        <h5 class="text-center text-success">Assy</h5>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-        {{--  --}}
-        <div class="row my-3">
-            <div class="col">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Total Emission Breakdown</h5>
-                        <div id="chart6"></div>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="row h-100">
-                    <div class="col">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Total Carbon in Energy</h5>
-                                <div class="mt-5">
-                                    <div id="chart7"></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Emission Metrics</h5>
-                                <table>
-                                    <thead>
-                                        <tr>
-                                            <th>BREAKDOWN</th>
-                                            <th class="text-center">THIS PERIOD</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>PAINTING</td>
-                                            <td class="d-flex flex-column align-items-center">
-                                                <p class="text-success">- 2%</p>
-                                                <p><small>16.200 TON</small></p>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>WELDING</td>
-                                            <td class="d-flex flex-column align-items-center">
-                                                <p class="text-danger">+ 14%</p>
-                                                <p><small>16.200 TON</small></p>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>PRESS</td>
-                                            <td class="d-flex flex-column align-items-center">
-                                                <p class="text-success">- 3%</p>
-                                                <p><small>16.200 TON</small></p>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>ASSY</td>
-                                            <td class="d-flex flex-column align-items-center">
-                                                <p class="text-danger">+ 20%</p>
-                                                <p><small>16.200 TON</small></p>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
+                        <h5 class="card-title">Emmissions by Plant</h5>
+                        <div id="chart3">
                         </div>
                     </div>
                 </div>
             </div>
         </div>
 
-
-        {{--  --}}
         <div class="row my-3">
-            <div class="col">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Average Emission Generated in Percentace</h5>
-                        <div id="chart8"></div>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Average Emission Generated</h5>
-                        <div id="chart9"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="row my-3">
-            <div class="col">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Total Emission/Offset</h5>
-                        <div id="chart4"></div>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
+            <div class="col-6">
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Total Emission Graph</h5>
-                        <div id="chart5"></div>
+                        <div id="chart-area"></div>
+                        <div id="chart-line"></div>
+                        <div id="chart-line2"></div>
                     </div>
                 </div>
             </div>
-            <div class="col">
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Total Emission/Offset</h5>
+                        <select id="" name="" class="form-select justify-content-end">
+                            <option value="all">All</option>
+                            <option value="plant1">Plant 1</option>
+                            <option value="plant2">Plant 2</option>
+                            <option value="plant3">Plant 3</option>
+                            <option value="plant4">Plant 4</option>
+                            <option value="plant5">Plant 5</option>
+                        </select>
+                        <div id="chart4"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Emission vs Target</h5>
+                        <select id="" name="" class="form-select justify-content-end">
+                            <option value="all">All</option>
+                            <option value="plant1">Plant 1</option>
+                            <option value="plant2">Plant 2</option>
+                            <option value="plant3">Plant 3</option>
+                            <option value="plant4">Plant 4</option>
+                            <option value="plant5">Plant 5</option>
+                        </select>
                         <p>daily</p>
                         <div class="progress">
                             <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40"
@@ -213,6 +95,7 @@
                                 40% Complete (success)
                             </div>
                         </div>
+                        <p class="justify-content-end">1000/7800</p>
                         <p>monthly</p>
                         <div class="progress">
                             <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="50"
@@ -220,6 +103,7 @@
                                 50% Complete (info)
                             </div>
                         </div>
+                        <p class="justify-content-end">1000/7800</p>
                         <p>yearly</p>
                         <div class="progress">
                             <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="60"
@@ -227,6 +111,7 @@
                                 60% Complete (warning)
                             </div>
                         </div>
+                        <p class="justify-content-end">1000/7800</p>
                     </div>
                 </div>
             </div>
